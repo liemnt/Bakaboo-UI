cf804f070cd573e29f46a2989af6ffe1ad3c1813
Nguyen Thanh Liem <liemnt.uit@gmail.com>
2017-03-06 20:02:48 +0700

    COmplete Index basic, need responsive

--- js/video.js
+++ js/video.js
@@ -7,5 +7,15 @@ $(document).ready(function () {
                 $("#introVideo").removeProp('muted', $("#introVideo").prop('muted'));
             }
         );
+        $("#btn-scroll-top img").click(
+            function(){
+                $('html, body').animate({
+                    scrollTop: $("#section-slideshow").offset().top
+                }, 500);
+            }
+        );
+    $("#btn-scroll-bot").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 500);
+    });
     }
 );
